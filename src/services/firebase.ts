5c7c19e7c972d2f9b90f97ec6b2a8201856c7ed3
pawelmat142 <pawelmat142@gmail.com>
2025-05-30 21:12:20 +0200

    env vars configuration

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,16 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, UserCredential, onAuthStateChanged, Unsubscribe, NextOrObserver, User, ErrorFn, CompleteFn } from "firebase/auth";
-import app from "../firebase/config";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJEKT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+const app = initializeApp(firebaseConfig)
 
 const auth = getAuth(app)
 
